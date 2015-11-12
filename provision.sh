37633efe632fb8369f0845ed12bbf82eee04ae20
golfhotel <webmaster@hosthello.com>
2015-11-12 23:10:46 +0100

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+	# Update all of the package references before installing anything
+	echo "Running apt-get update..."
+	apt-get update -y
+	echo -e "Installo Git." 
+	sudo apt-get install -y git
+	
 # controlla se esiste SIS.
-if [[ ! -d /home/vagrant/sis-1.3.6.bin64 ]]; then
-	echo -e "\nSposto cartella sis"
+if [[ ! -d /home/vagrant/sis-1.3.6-binx64 ]]; then
+	echo -e "\nInstallo Sis"
 	cd /home/vagrant
 	git clone https://github.com/golfhotel/sis-1.3.6-binx64.git
 else
@@ -10,4 +16,4 @@ else
 fi
 
 # aggiunge la variabile d'ambiente nel file .bashrc
-echo 'export PATH=$PATH:/home/vagrant/sis-1.3.6.bin64/bin/' >> ~/.bashrc 
+echo 'export PATH=$PATH:/home/vagrant/sis-1.3.6-binx64/sis-1.3.6.bin64/bin/' >> ~/.bashrc 
